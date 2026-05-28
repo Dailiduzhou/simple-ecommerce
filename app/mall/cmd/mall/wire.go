76d57ff2e069ed1e2217415edc016ce8a13eb93a
dailiduzhou <qzhsdream@163.com>
2026-05-28 08:38:30 +0800

    chore: framework

---
+++ app/mall/cmd/mall/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/Dailiduzhou/simple-ecommerce/app/app/mall/internal/biz"
+	"github.com/Dailiduzhou/simple-ecommerce/app/app/mall/internal/conf"
+	"github.com/Dailiduzhou/simple-ecommerce/app/app/mall/internal/data"
+	"github.com/Dailiduzhou/simple-ecommerce/app/app/mall/internal/server"
+	"github.com/Dailiduzhou/simple-ecommerce/app/app/mall/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Dailiduzhou/simple-ecommerce
+
+go 1.26.3
